36b5b33e686d8a0444df88957ac50206f5d6b309
krissdrawing <krx95@o2.pl>
2021-05-20 18:52:58 +0200

    Added theme with colors and screen sizes

---
+++ src/Themes/mainTheme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    primary: "#FABD20",
+    secondary: "#FFFFFF",
+    tertiary: "#131E07",
+    fontPrimaryDark: "#131E07",
+    fontPirmaryLight: "#40462F",
+    fontSecondaryDark: "#7C8176",
+    fontSecondaryLight: "#E5E5E5",
+  },
+  screenSize: {
+    s: "320px",
+    md: "768px",
+  },
+};
+
+export { theme };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Themes/mainTheme";
 import GlobalStyle from "./Themes/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
