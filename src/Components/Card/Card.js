dbbbfcddd61b5f24a6a779800f99bb67d1518c24
krissdrawing <krx95@o2.pl>
2021-05-21 21:18:21 +0200

    Created Card to offert section

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { H4, Paragraph } from "Components/Utility/Utility";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-width: 327px;
+  max-width: 350px;
+  height: 439px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 42px 0 45px 0;
+  margin: 20px 15px;
+  text-align: center;
+`;
+const Img = styled.img`
+  margin-bottom: 81px;
+`;
+
+const StyledParagraph = styled(Paragraph)`
+  margin-top: 16px;
+  font-size: 18px !important;
+  line-height: 27px !important;
+`;
+
+const Card = ({ src, heading, children }) => {
+  return (
+    <Wrapper>
+      <Img src={src} />
+      <H4>{heading}</H4>
+      <StyledParagraph>{children}</StyledParagraph>
+    </Wrapper>
+  );
+};
+Card.propTypes = {
+  src: PropTypes.node.isRequired,
+  heading: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Card;
