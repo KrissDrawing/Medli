f92d5de98c010f1c33ac85441f4e5b4db7061a1b
krissdrawing <krx95@o2.pl>
2021-05-21 20:45:56 +0200

    Added mock-up of videoPlayer

---
+++ src/Components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import playButton from "./Play.svg";
+
+const Wrapper = styled.section`
+  width: 100%;
+  height: 160px;
+  position: relative;
+  border-radius: 8px;
+  overflow: hidden;
+  align-self: center;
+
+  @media (min-width: ${({ theme }) => theme.screenSize.md}) {
+    height: 603px;
+    width: 1240px;
+  }
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  & > img {
+    @media (min-width: ${({ theme }) => theme.screenSize.md}) {
+      width: 130px;
+      height: 130px;
+    }
+  }
+`;
+
+const VideoImg = styled.img`
+  width: 100%;
+`;
+
+const VideoPlayer = ({ src }) => {
+  return (
+    <Wrapper>
+      <VideoImg src={src} />
+      <StyledButton>
+        <img src={playButton} />
+      </StyledButton>
+    </Wrapper>
+  );
+};
+
+VideoPlayer.propTypes = {
+  src: PropTypes.node.isRequired,
+};
+
+export default VideoPlayer;
