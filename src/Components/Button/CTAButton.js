9e035828b6ccb4376b60a23276346ac9dab2db5a
krissdrawing <krx95@o2.pl>
2021-05-21 10:14:03 +0200

    Added proptypes to CTAButton

--- src/Components/Button/CTAButton.js
+++ src/Components/Button/CTAButton.js
@@ -1,14 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { PrimaryButton } from "Components/Utility/Utility";
 
 const StyledButton = styled.button`
   ${PrimaryButton};
+  height: 62px;
+  width: 100%;
   font-size: ${({ size }) => (size === "small" ? "20x" : "25px")};
   border-radius: ${({ size }) => (size === "small" ? "4px" : "8px")};
   background-color: ${({ theme }) => theme.colors.primary};
-  height: 62px;
-  width: 100%;
 
   @media (min-width: ${({ theme }) => theme.screenSize.md}) {
     width: ${({ size }) => (size === "small" ? "113px" : "255px")};
@@ -22,6 +23,11 @@ const CTAButton = ({ children, size }) => {
       {children}
     </StyledButton>
   );
+};
+
+CTAButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  size: PropTypes.string,
 };
 
 export default CTAButton;
