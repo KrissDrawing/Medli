ce4eca5690e25d893939c96d3621539e8d6eddb6
krissdrawing <krx95@o2.pl>
2021-05-21 13:30:44 +0200

    Added Headers to utlity file

--- src/Components/Utility/Utility.js
+++ src/Components/Utility/Utility.js
@@ -4,3 +4,24 @@ export const PrimaryButton = css`
   background-color: ${({ theme }) => theme.colors.primary};
   border-radius: 8px;
 `;
+
+export const H2 = styled.h2`
+  font-family: "Rozha One", serif;
+  color: ${({ theme }) => theme.colors.fontPrimaryDark};
+  font-size: 50px;
+
+  @media (min-width: ${({ theme }) => theme.screenSize.md}) {
+    font-size: 80px;
+  }
+`;
+
+export const H2Light = styled.h2`
+  font-family: "Rozha One", serif;
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 80px;
+`;
+
+export const H4 = styled.h4`
+  font-family: "Rozha One", serif;
+  font-size: 24px;
+`;
